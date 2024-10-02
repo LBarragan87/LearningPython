1cbd6ebe69b546b50da5af0168542160b931d166
LBarragan87 <luis.barragan.huerta@hotmail.com>
2024-10-02 16:58:52 -0600

    Aprendiendo Inputs

---
+++ 01_BASICO/SECCION_01/03_Inputs/S1_03_02_Practica1.py
@@ -0,0 +1,18 @@
+'''
+Crea un código que le permita ingresar una respuesta al usuario, haciéndole la siguiente pregunta:
+
+¿Qué estás estudiando?
+
+El código debe poder imprimir en pantalla lo ingresado por el usuario (utilizando print).
+
+Aclaración:
+
+1. No deben imprimirse strings adicionales a la respuesta del usuario.
+
+2. Los ejercicios que contienen el la función input() deben ser probados con el botón: "Ejecutar pruebas".  Ya que el botón "Ecutar código" arrojará el siguiente error: "EOF when reading a line".
+
+¿Aún tienes dudas acerca de por qué el módulo de evaluación no acepta tu respuesta? Hemos incluido al final de cada sección posibles respuestas a cada uno de los ejercicios. Confiamos en que puedas recurrir a ellas para detectar diferencias rápidamente, evitando que pierdas el tiempo o debas esperar por una respuesta a tu pregunta. Si aún así tienes dudas a resolver, puede ser una buena oportunidad para repasar la lección en cuestión, o utilizar el espacio de Preguntas y Respuestas que tienes a tu disposición. Muchos éxitos!
+'''
+
+estudio = input ("¿Qué estás estudiando?")
+print (estudio)
