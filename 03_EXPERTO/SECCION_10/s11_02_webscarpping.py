5bb98d8adb256cbd5a374dac4be0eaf6eacb9401
LBarragan87 <luis.barragan.huerta@hotmail.com>
2024-10-16 12:52:49 -0600

    actualizacion_proyecto9

--- 03_EXPERTO/SECCION_10/s11_02_webscarpping.py
+++ 03_EXPERTO/SECCION_10/s11_02_webscarpping.py
@@ -2,7 +2,8 @@ import bs4
 import requests
 
 
-resultado = requests.get("https://escueladirecta-blog.blogspot.com/2021/10/encapsulamiento-pilares-de-la.html")
+resultado = requests.get("https://escueladirecta-blog.blogspot.com/2021/10/"
+                         "encapsulamiento-pilares-de-la.html")
 
 
 sopa = bs4.BeautifulSoup(resultado.text, "lxml")
